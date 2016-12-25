2a63d7b6d1255065e0f68ba50662fc21369e0078
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2016-12-25 12:07:36 +0530

    UWP Hamburger Menu
    UWP Hamburger Menu with Frosted glass effect and Swipe to Open/Close

---
+++ HamburgerMenu/HamburgerMenu/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Animations;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+//Created and Designed by Vijay Nirmal
+
+namespace HamburgerMenu
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamBurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            HamburgerMenu.IsPaneOpen = !HamburgerMenu.IsPaneOpen;
+        }
+
+        private void HamburgerMenuListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SplitViewPane_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (e.Cumulative.Translation.X < -50)
+            {
+                HamburgerMenu.IsPaneOpen = false;
+            }
+
+            if (e.Cumulative.Translation.X > 50)
+            {
+                HamburgerMenu.IsPaneOpen = true;
+            }
+        }
+
+        private void SplitViewOpener_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (e.Cumulative.Translation.X > 50)
+            {
+                HamburgerMenu.IsPaneOpen = true;
+            }
+        }
+    }
+}
